1e93f1774cf34388c58874560252e27cac3c199d
charlie <xyhp915@qq.com>
2023-07-24 15:17:03 +0800

    feat: WIP plans components for the pro page

--- src/pages/Pro/index.tsx
+++ src/pages/Pro/index.tsx
@@ -1,6 +1,6 @@
 import './index.css'
 import cx from 'classnames'
-import { FileCloud, LockKeyOpen, ShieldStar } from 'phosphor-react'
+import { CircleWavyQuestion, FileCloud, Files, LockKeyOpen, Notebook, ShieldStar, Stack, Student } from 'phosphor-react'
 
 function ProCard ({ children, className, ...rest }: any) {
   return (
@@ -23,7 +23,7 @@ function ProInfoSection () {
       </div>
 
       {/*price card*/}
-      <div className={'flex pt-14 space-x-6 z-10'}>
+      <div className={'flex pt-14 space-x-6 z-10 relative pb-[190px]'}>
         <ProCard className={'plans-card a flex-col flex-1'}>
           <i><FileCloud size={38} weight={'duotone'}/></i>
           <h1 className={'text-4xl'}>
@@ -32,7 +32,7 @@ function ProInfoSection () {
           <h2 className={'text-xl'}>
             <b>Benefit from our</b> smart merging system for seamless syncing,
             <b className={'ml-2'}>as well as</b> expanded storage and larger <br/>asset support
-            <b className={'ml-2'}>across all devices</b>.
+            <b className={'ml-2'}>across all devices.</b>
           </h2>
           <h3 className={'flex items-center bg-pro-900/90 rounded-lg mt-4 py-2 px-4 leading-none'}>
             <ShieldStar weight={'duotone'} size={32}/>
@@ -50,8 +50,51 @@ function ProInfoSection () {
           </h1>
           <h2 className={'text-xl'}>
             <b>Be the first to upgrade your workflows with</b> exclusive <br/>access to beta features,
-            <b className={'ml-2'}>helping shape the future of <br/>Logseq</b>.
+            <b className={'ml-2'}>helping shape the future of <br/>Logseq.</b>
           </h2>
+          <section className="big-things">
+            <h4>NEXT BIG THINGS</h4>
+            <ul>
+              <li>
+                <strong>UX/UI Refresh</strong>
+                <span>Enhanced Navigation & Design: <b>Intuitive and visually appealing.</b></span>
+              </li>
+              <li>
+                <strong>Logseq Publish</strong>
+                <span>Effortless Note Sharing: <b>Publish your thoughts with ease.</b></span>
+              </li>
+              <li>
+                <strong>Real-time collaboration</strong>
+                <span>Empower Teamwork: <b>Work simultaneously and boost productivity.</b></span>
+              </li>
+            </ul>
+          </section>
+        </ProCard>
+
+        <ProCard className={'plans-card c'}>
+          <span>
+            <Stack size={36} weight={'duotone'} color={'#3cbaf3'}/>
+            <strong>100GB</strong>
+            <small>total storage</small>
+          </span>
+          <span>
+            <Notebook size={36} weight={'duotone'} color={'#3cbaf3'}/>
+            <strong>10GB</strong>
+            <small>per graph</small>
+          </span>
+          <span>
+            <Files size={36} weight={'duotone'} color={'#3cbaf3'}/>
+            <strong>100MB</strong>
+            <small>per asset</small>
+          </span>
+          <span>
+            <Student size={40} weight={'duotone'} color={'#3cbaf3'}/>
+            <p className={'flex flex-col pl-2'}>
+              <strong className={'flex text-xl font-medium items-center'}>Student pricing
+                <i className={'ml-1 text-pro-200'}><CircleWavyQuestion/></i></strong>
+              <small>Contact us</small>
+            </p>
+          </span>
         </ProCard>
       </div>
 
